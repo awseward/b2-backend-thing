44b39e217f8256ced9658636b471fef212a2d0d5
Andrew Seward <awswrd@gmail.com>
2021-08-22 14:10:17 -0700

    Replace btoa
    Apparently it's deprecated 🤷…
    
    Also, added a little "extend express.Request" idea that I was hoping
    would layer some types on the request coming through, but it doesn't
    seem to have done that.

--- src/index.ts
+++ src/index.ts
@@ -5,13 +5,17 @@ interface AppCred {
   key: string;
 };
 
-export const mkAuthHeader = ({ keyId, key }: AppCred) =>
-  `Basic ${btoa(`${keyId}:${key}`)}`;
-
 interface UrlParts {
   apiUrl: string; // returned by b2_authorize_account
   versionNumber: number;
   apiName: string;
+}
+
+interface Req<T> extends express.Request { body: T }
+
+export const mkAuthHeader = ({ keyId, key }: AppCred) => {
+  const encoded = Buffer.from(`${keyId}:${key}`).toString('base64');
+  return `Basic ${encoded}`;
 }
 
 export const mkUrl = ({ apiUrl, versionNumber, apiName }: UrlParts) =>
@@ -22,14 +26,10 @@ const port = 5001;
 
 app.use(express.json());
 
-app.get('/', (_, res) => {
-  res.status(200).send();
-});
-
 // curl -XPOST localhost:5001/get_upload_url -H 'Content-Type: application/json' -d '{ "keyId": "foo", "key": "bar" }'
-app.post('/get_upload_url', (req, res) => {
+app.post('/get_upload_url', (req: Req<AppCred>, res) => {
   // TODO: Figure out how to make this error if the req body doesn't match the interface we're expecting…
-  res.status(501).send(mkAuthHeader(req.body));
+  res.status(501).send(`TODO: ${mkAuthHeader(req.body)}\n`);
 });
 
 app.listen(port, () => console.log(`Running on port ${port}`));
