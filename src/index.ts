d70c0e940f77d6a8c119e5681aadea8594c18906
Andrew Seward <awswrd@gmail.com>
2021-08-22 15:01:19 -0700

    Get to an MVP
    This is actually in a state for a client to call it and get what's
    needed to upload some files.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import express from 'express'
 
 interface AppCred {
@@ -11,25 +12,85 @@ interface UrlParts {
   apiName: string;
 }
 
-interface Req<T> extends express.Request { body: T }
+const mkB2Url = ({ apiUrl, versionNumber, apiName }: UrlParts) =>
+  `${apiUrl}/b2api/v${versionNumber}/${apiName}`;
 
-export const mkAuthHeader = ({ keyId, key }: AppCred) => {
-  const encoded = Buffer.from(`${keyId}:${key}`).toString('base64');
-  return `Basic ${encoded}`;
+const handleAxiosError = (e: any) => {
+  if (e.response) {
+    // The request was made and the server responded with a status code
+    // that falls out of the range of 2xx
+    console.log(e.response.data);
+    console.log(e.response.status);
+    console.log(e.response.headers);
+  } else if (e.request) {
+    // The request was made but no response was received
+    // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+    // http.ClientRequest in node.js
+    console.log(e.request);
+  } else {
+    // Something happened in setting up the request that triggered an Error
+    console.log('Error', e.message);
+  }
+  console.log(e.config);
+
+  throw e;
 }
 
-export const mkUrl = ({ apiUrl, versionNumber, apiName }: UrlParts) =>
-  `${apiUrl}/b2api/v${versionNumber}/${apiName}`;
+const b2AuthorizeAccount = ({ keyId, key }: AppCred) : Promise<any> =>
+  axios.get(
+    'https://api.backblazeb2.com/b2api/v2/b2_authorize_account',
+    {
+      auth: {
+        username: keyId,
+        password: key
+      }
+    }
+  ).catch(handleAxiosError);
+
+interface B2GetUploadUrlResponse {
+  authorizationToken: string,
+  bucketId: string,
+  uploadUrl: string
+}
+
+const b2GetUploadUrl = (
+  urlParts: UrlParts,
+  authToken: string,
+  bucketId: string
+) : Promise<any> =>
+  axios.post(
+    mkB2Url({ ...urlParts, apiName: 'b2_get_upload_url' }),
+    { bucketId },
+    {
+      headers: { 'Authorization': authToken },
+    }
+  ).catch(handleAxiosError);
+
+interface Req<T> extends express.Request { body: T }
 
 const app = express();
 const port = 5001;
-
 app.use(express.json());
 
-// curl -XPOST localhost:5001/get_upload_url -H 'Content-Type: application/json' -d '{ "keyId": "foo", "key": "bar" }'
-app.post('/get_upload_url', (req: Req<AppCred>, res) => {
+app.post('/get_upload_url', async (req: Req<AppCred>, res) => {
   // TODO: Figure out how to make this error if the req body doesn't match the interface we're expecting…
-  res.status(501).send(`TODO: ${mkAuthHeader(req.body)}\n`);
+  const authznRes = await b2AuthorizeAccount(req.body);
+  const {
+    allowed: { bucketId },
+    apiUrl,
+    authorizationToken,
+  } = authznRes.data;
+  const uploadUrlRes = await b2GetUploadUrl(
+    {
+      apiUrl,
+      versionNumber: 2,
+      apiName: '', // Not actually used in this call -- probably going to remove from the interface and shape things a little differently…
+    },
+    authorizationToken,
+    bucketId
+  );
+
+  res.status(200).send(uploadUrlRes.data as B2GetUploadUrlResponse);
 });
 
 app.listen(port, () => console.log(`Running on port ${port}`));
